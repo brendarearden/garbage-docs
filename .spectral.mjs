59642b28ed6d740d5ca9c1383177d586980dfcdd
Brenda Rearden <brendarearden@gmail.com>
2024-01-10 09:01:49 -0700

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight-local.com:8443/api/v1/projects/cHJqOjY0/spectral.js?branch_slug=main&token=7b560ab4-a934-43b7-8610-a43a5418b65a";
+export default { extends: ruleset };
